13f07ee1640f4d90a428b8125417a0cf75bbc5c9
Alexandr Kolymago <kolymago.a@gmail.com>
2016-06-24 23:18:39 +0300

    Task 1.2 done.

--- 03-Data Types/arrays.js
+++ 03-Data Types/arrays.js
@@ -8,3 +8,13 @@ var array = (function () {
 	console.log(_.range(20).map(function (o, i) { return i * 5; }));
 }());
 
+/**
+ * Task 2
+ */
+var charArrayOne = ['a', 'b', 'c'],
+	charArrayTwo = ['c', 'd', 'e'],
+	charArrayThree = ['a', 'b', 'c'];
+
+var isSame = _.isEqual(charArrayOne, charArrayTwo);
+
+console.log(isSame ? "Arrays are equal." : "Arrays are not equal.");
