dc9b2df1413071e91f0cb88f46adc549454b58b8
Alexandr Kolymago <kolymago.a@gmail.com>
2016-06-25 01:47:51 +0300

    Task 3.1 done.

---
+++ 03-Data Types/objects.js
@@ -0,0 +1,48 @@
+/*jslint devel: true, nomen: true, plusplus: true, sloppy: true*/
+/* =============== Objects =============== */
+
+/**
+ * Task 1
+ */
+var p1 = {
+		'x': 10,
+		'y': 5
+	},
+	p2 = {
+		'x': 15,
+		'y': 1
+	},
+	lineA = {
+		'pointA': { 'x': 1, 'y': 1 },
+		'pointB': { 'x': 1, 'y': 4 }
+	},
+	lineB = {
+		'pointA': { 'x': 1, 'y': 1 },
+		'pointB': { 'x': 3, 'y': 1 }
+	},
+	lineC = {
+		'pointA': { 'x': 3, 'y': 1 },
+		'pointB': { 'x': 1, 'y': 4 }
+	};
+
+var getDistanceBetween = function (pointA, pointB) {
+	return "Distance between points are = " +
+		Math.sqrt(Math.pow(pointB.x - pointA.x, 2) + Math.pow(pointB.y - pointA.y, 2));
+};
+
+console.log(getDistanceBetween(p1, p2));
+
+var canFormTriangle = function (lineA, lineB, lineC) {
+	var a, b, c;
+	a = getDistanceBetween(lineA.pointA, lineA.pointB);
+	b = getDistanceBetween(lineB.pointA, lineB.pointB);
+	c = getDistanceBetween(lineC.pointA, lineC.pointB);
+	console.log(a, b, c);
+	if ((a + b > c) && (a + c > b) && (b + c > a)) {
+		return "Lines can form triangle";
+	} else {
+		return "Lines can't form triangle";
+	}
+};
+
+console.log(canFormTriangle(lineA, lineB, lineC));
