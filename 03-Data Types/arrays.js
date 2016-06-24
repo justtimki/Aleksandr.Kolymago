1ce87e6bcbf6ce0d5915aa61cb98341f11361b11
Alexandr Kolymago <kolymago.a@gmail.com>
2016-06-25 00:38:28 +0300

    Task 1.3 and 1.4 done.

--- 03-Data Types/arrays.js
+++ 03-Data Types/arrays.js
@@ -18,3 +18,40 @@ var charArrayOne = ['a', 'b', 'c'],
 var isSame = _.isEqual(charArrayOne, charArrayTwo);
 
 console.log(isSame ? "Arrays are equal." : "Arrays are not equal.");
+
+/**
+ * Task 3
+ */
+var sourceArray = [2, 1, 1, 2, 3, 3, 2, 2, 2, 1];
+
+var getMaxSequenceOf = function (array, option) {
+	var repeatCount = 0,
+		repeatStartPos,
+		repeatEndPos,
+		resultArray = [],
+		i;
+	for (i = 0; i < array.length; i++) {
+		if (option(array[i], array[i + 1])) {
+			if (repeatCount === 0) {
+				repeatStartPos = i;
+			}
+			repeatCount++;
+		} else {
+			repeatEndPos = i + 1;
+			if (repeatCount >= resultArray.length) {
+				resultArray = array.slice(repeatStartPos, repeatEndPos);
+			}
+			repeatCount = 0;
+		}
+	}
+	return resultArray;
+};
+
+console.log(getMaxSequenceOf(sourceArray, function (a, b) { return a === b; }));
+
+/**
+ * Task 4
+ * In this task I reuse my fuction from task 3.
+ */
+var sourceArray = [3, 2, 3, 4, 2, 2, 4];
+console.log(getMaxSequenceOf(sourceArray, function (a, b) { return (b - a) === 1; }));
