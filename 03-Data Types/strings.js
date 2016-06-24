f4840ec2c9115c49764e3a18e1e3d065b975bf33
Alexandr Kolymago <kolymago.a@gmail.com>
2016-06-25 02:31:43 +0300

    Task 4.2 done.

--- 03-Data Types/strings.js
+++ 03-Data Types/strings.js
@@ -5,7 +5,32 @@
  * Task 1
  */
 var reverseString = function (string) {
+	string = string || '';
 	return string.split('').reverse().join('');
 };
 
 console.log(reverseString('Hello'));
+
+/**
+ * Task 2
+ */
+var expression = '(a+b))';
+
+var isBracketsCorrectly = function (expression) {
+	expression = expression || '';
+	var i,
+		count = 0;
+	if (expression[0] === ')') {
+		return false;
+	}
+	for (i = 0; i < expression.length; i++) {
+		if (expression[i] === '(') {
+			count++;
+		}
+		if (expression[i] === ')') {
+			count--;
+		}
+	}
+	return (count === 0);
+};
+console.log("Expression", expression, "is", isBracketsCorrectly(expression));
