89a1f8ff8be674350baf08e2786d99336acedbcb
Alexandr Kolymago <kolymago.a@gmail.com>
2016-06-24 23:05:08 +0300

    Task 1.1 done.

---
+++ 03-Data Types/arrays.js
@@ -0,0 +1,10 @@
+/*jslint devel: true, nomen: true, plusplus: true, sloppy: true*/
+/* =============== Arrays =============== */
+
+/**
+ * Task 1
+ */
+var array = (function () {
+	console.log(_.range(20).map(function (o, i) { return i * 5; }));
+}());
+
